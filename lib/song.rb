97593f96189dfea418efd1857db049cb04787dab
Indre Bakeer <indre.bakeer@gmail.com>
2019-02-08 20:43:31 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,8 +6,7 @@ class Song
   end
 
   def self.new_by_filename(filename)
-    song = self.new(filename)
-    self.song = filename.split(" - ")[1]
+    self.new(name) = filename.split(" - ")[1]
   end
 
 end
